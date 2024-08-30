debffadc9f020d6738ecdf20ebd877b9b9391d78
Raouldepaste <raoulivan81@gmail.com>
2024-08-30 12:16:50 +0300

    small tweaks, app still not working

--- LangchainTutorialSetup/indexing.py
+++ LangchainTutorialSetup/indexing.py
@@ -18,6 +18,6 @@ from langchain_chroma import Chroma
 from langchain_openai import OpenAIEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=200)
 splits = text_splitter.split_documents(docs)
-vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings(), persist_directory=".\\embeddedBookDB")
+vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings(), persist_directory=".\\embeddedBooksDB")
